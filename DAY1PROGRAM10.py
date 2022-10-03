da377b95c73774c3f81ff285522ae2fc54fbc56d
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-03 13:22:13 +0530

    Add files via upload

--- DAY1PROGRAM10.py
+++ DAY1PROGRAM10.py
@@ -1,44 +1,10 @@
-def addFrequencyToCharacter(s):
-	
-	
-	frequency = [0] * 26
+str=input("Enter the value: ")
+new_str=''
+list=[]
+for i in range(0,len(str)):
+    list.append(str.count(str[i]))
+    new_val=ord(str[i])
+    print(chr(new_val+list[i]),end="")
 
-	# Stores length of the string
-	n = len(s)
-
-	# Traverse the given string S
-	for i in range(n):
-		
-		
-		frequency[ord(s[i]) - ord('a')] += 1
-
-	# Traverse the string
-	for i in range(n):
-		
-		# Store the value to be added
-		# to the current character
-		add = frequency[ord(s[i]) - ord('a')] % 26
-
-		# Check if after adding the
-		# frequency, the character is
-		# less than 'z' or not
-		if (ord(s[i]) + add <= ord('z')):
-			s[i] = chr(ord(s[i]) + add)
-
-		# Otherwise, update the value of
-		# add so that s[i] doesn't exceed 'z'
-		else:
-			add = (ord(s[i]) + add) - (ord('z'))
-			s[i] = chr(ord('a') + add - 1)
-
-	# Print the modified string
-	print("".join(s))
-
-# Driver Code
-if __name__ == '__main__':
-	
-	str = "lion"
-	
-	addFrequencyToCharacter([i for i in str])
 	
 
