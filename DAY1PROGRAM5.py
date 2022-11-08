0c5af6647b93f5f09166d56788cf3001676e47f1
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-11-08 11:29:30 +0530

    Update DAY1PROGRAM5.py

--- DAY1PROGRAM5.py
+++ DAY1PROGRAM5.py
@@ -1,31 +1,12 @@
-#v1
-fresh_loaf = 185
-discount = 60
+a=int(input("Enter the number of fresh loaves purchased :"))
+b=int(input("Enter the number of old loaves purchased :"))
+if(a<=0):
+    print("Enter a positive integer greater than 0")
+else:
+    f=a*185
+    o=(b*185)*60/100
+    print("Regular price: Rs.185.00")
+    print("Amount of new loaves:", float(f))
+    print("Amount of old loaves:", float(o))
+    print("Total Amount:", f+o)
 
-def message():
-    amount = float(input("ENTER THE NUMBER OF FRESH LOAVES PURCHASED: "))
-    regular = round(fresh_loaf * amount,2)
-    discounted = round(regular * discount,2)
-    total = round(regular - discounted,2)
-    print(f'REGULAR PRICE is {regular}\nAMOUNT OF NEW LOAVES{total}')
-
-message()
-
-#v2
-fresh_loaf = 185
-discount = 60
-
-def message():
-    amount = input("Please enter loaves of yesterday's bread (q to quit): ")
-    while True:
-        if amount == 'q':
-            break
-        elif amount != '' and amount != ' ':
-            amount = float(amount)
-            regular = round(fresh_loaf * amount,2)
-            discounted = round(regular * discount,2)
-            total = round(regular - discounted,2)
-            print(f'Regular price is {regular} \nThe discount is {discounted}\nTotal is {total}')
-        amount = input("Please enter loaves of yesterday's bread(q to quit): ")
-
-message()
